ac839340203189d86232b9ab3474fc5769757d01
juanmijares3487-commits <juan.mijares3487@alumnos.udg.mx>
2026-05-06 08:30:45 -0600

    primer commit

--- Viernes13Meyo/MayorEdad.c
+++ Viernes13Meyo/MayorEdad.c
@@ -1,1 +1,14 @@
 #include <stdio.h>
+
+int main(){
+
+    int edad;
+
+    printf("Ingresa tu edad: ");
+    scanf ("%d", edad);
+    if(edad <= 18)
+        printf("Eres mayor de edad");
+    else
+        printf("Todavia estas bien verde");
+             
+}
